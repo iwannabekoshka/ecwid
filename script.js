2e3a789b00a9f9b6afe2d213a6b10eda2139bff3
pokusaeff.daniil <69844034+iwannabekoshka@users.noreply.github.com>
2021-03-10 07:25:37 +0300

    init 2

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log(`allo`)
+// Initialize the application
+EcwidApp.init({
+	app_id: "yasnaya-polyana", // your client_id
+	autoloadedflag: true,
+	autoheight: true
+});
+
+// Get the store ID and access token
+const storeData = EcwidApp.getPayload();
+const storeId = storeData.store_id;
+const accessToken = storeData.access_token;
+const language = storeData.lang;
+
+if (storeData.public_token !== undefined){
+	const publicToken = storeData.public_token;
+}
+
+if (storeData.app_state !== undefined){
+	const appState = storeData.app_state;
+}
